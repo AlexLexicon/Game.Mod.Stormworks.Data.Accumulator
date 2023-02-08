6993be59485ee67b062e441caff3e29af13282fb
Alex Stroot <alexrs.business@outlook.com>
2023-02-07 18:51:43 -0800

    replaced wpf with console app

--- Game.Mod.Stormworks.Data.Accumulator.ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Game.Mod.Stormworks.Data.Accumulator.ViewModels;
-public partial class MainWindowViewModel : ObservableObject
-{
-
-}
--- Game.Mod.Stormworks.Data.Accumulator.Views.Wpf/MainWindowView.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using Game.Mod.Stormworks.Data.Accumulator.ViewModels;
-using System.Windows;
-
-namespace Game.Mod.Stormworks.Data.Accumulator.Views.Wpf;
-public partial class MainWindowView : Window
-{
-    public MainWindowView() => InitializeComponent();
-}
